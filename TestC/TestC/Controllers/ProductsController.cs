cd30ada22a011ac65f4217a9c2bc9d4265688674
Lab. Desenvolvimento <Lab. Desenvolvimento@RB-LAB101-11>
2017-09-12 10:53:11 -0300

    Commit inicial

---
+++ TestC/TestC/Controllers/ProductsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestC.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View(new List<Product>());
+        }
+    }
+}
---
+++ TestC/TestC/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace TestC.Controllers
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Brand { get; set; }
+        public int Price { get; set; }
+    }
+}
