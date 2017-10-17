1c018b12f283ce9c2ea584eeb0c24710643957c2
Vincius Nadalin de Almeida <nadalinvini@gmail.com>
2017-10-17 10:49:00 -0200

    Xd

--- TestC/TestC/Models/Product.cs
+++ TestC/TestC/Models/Product.cs
@@ -2,10 +2,17 @@
 {
     public class Product
     {
-        public int ProductId { get; set; }
+        public long? ProductId { get; set; }
         public string Name { get; set; }
         public string Type { get; set; }
         public string Brand { get; set; }
         public int Price { get; set; }
+
+        public long? StoreId { get; set; }
+        public long? ClientId { get; set; }
+
+        public Client Client { get; set; }
+        public Store Store { get; set; }
+
     }
 }
