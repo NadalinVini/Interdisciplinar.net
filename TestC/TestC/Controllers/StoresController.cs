b785a4aeeb6b3f13711824d8828d7711b5542e26
Aldryn <Lab. Desenvolvimento@RB-LAB101-10>
2017-11-07 10:06:01 -0200

    isso AI

--- TestC/TestC/Controllers/StoresController.cs
+++ TestC/TestC/Controllers/StoresController.cs
@@ -1,6 +1,9 @@
-﻿using System.Data.Entity;
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Net;
+using System.Web;
 using System.Web.Mvc;
 using TestC.Context;
 using TestC.Models;
@@ -10,12 +13,57 @@ namespace TestC.Controllers
     public class StoresController : Controller
     {
         private EFContext context = new EFContext();
-        // GET: Clients
+        // GET: Stores
         public ActionResult Index()
         {
             return View(context.Stores.OrderBy(
             c => c.Name));
-        }  
+        }
+        // GET: Stores/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Store store)
+        {
+            context.Stores.Add(store);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        //	GET:Stores/Edit
+        public ActionResult Edit(long? id)
+        {
+            if (id == null)
+            {
+                return new
+                    HttpStatusCodeResult(
+                    HttpStatusCode.BadRequest);
+            }
+            Store store = context.Stores.Find(id);
+            if (store == null)
+            {
+                return HttpNotFound();
+            }
+            return View(store);
+        }
+
+        //	POST: Stores/Edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Store store)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Entry(store).State =
+                                                   EntityState.Modified;
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(store);
+        }
         //	GET: Details
         public ActionResult Details(long? id)
         {
@@ -32,8 +80,35 @@ namespace TestC.Controllers
             }
             return View(store);
         }
-        
-       
+        //	GET:	Stores/Delete
+        public ActionResult Delete(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(
+                                HttpStatusCode.BadRequest);
+            }
+            Store store = context.Stores.
+                Find(id);
+            if (store == null)
+            {
+                return HttpNotFound();
+            }
+            return View(store);
+        }
+        //	POST:	Stores/Delete
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(long id)
+        {
+            Store store = context.Stores.
+                            Find(id);
+            context.Stores.Remove(store);
+            context.SaveChanges();
+            TempData["Message"] = "Store	" +
+                                store.Name.ToUpper() + "	was	removed";
+            return RedirectToAction("Index");
+        }
 
 
     }
